01d4a53cb5ac6512f8454d89cb6b92ef56b1dea7
kader130001-dev <kader130001@hotmail.fr>
2026-04-07 21:38:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,8 +68,8 @@ async def unlock(ctx):
 async def derank(ctx, member: discord.Member):
     roles = [role for role in member.roles if role != ctx.guild.default_role]
     await member.remove_roles(*roles)
-    await ctx.send(f"✅ Tous les rôles de {member.mention} ont été retirés.")
-
+    await ctx.send(f"🔨 {member.mention} a été derank !")
+    
 @bot.command()
 async def commandes(ctx):
     embed = discord.Embed(title="📋 Commandes disponibles", color=0x00bfff)
