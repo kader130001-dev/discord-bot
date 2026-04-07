ca4246750fadc0b6bf7452af8ad081f09de312b5
kader130001-dev <kader130001@hotmail.fr>
2026-04-07 15:08:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="+", intents=intents)
 
 @bot.command()
 @commands.has_permissions(kick_members=True)
