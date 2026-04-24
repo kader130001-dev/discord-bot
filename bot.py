9af92a21a88fdaef717bf81159499a20b018a646
kader130001-dev <kader130001@hotmail.fr>
2026-04-25 01:51:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,19 @@ bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
-    try:
-        await bot.load_extension("cogs.gestion")
-    except Exception as e:
-        print(f"Erreur cog: {e}")
+    COGS = [
+        "cogs.gestion",
+        "cogs.utils",
+        "cogs.logs",
+        "cogs.tickets",
+        "cogs.antiraid"
+    ]
+    for cog in COGS:
+        try:
+            await bot.load_extension(cog)
+            print(f"✅ Cog chargé : {cog}")
+        except Exception as e:
+            print(f"❌ Erreur sur {cog} : {e}")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="le serveur"))
     print(f"✅ {bot.user} connecté.")
 
