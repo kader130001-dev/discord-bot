f543ea7ca9b8962e3078a3a615aef726a8dd33d2
kader130001-dev <kader130001@hotmail.fr>
2026-04-24 08:20:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,6 @@ PREFIX = "+"
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
-bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
-bot.remove_command("help")  # ← ajoute ça
 
 # ══════════════════════════════════════════
 #  BASE DE DONNÉES JSON
